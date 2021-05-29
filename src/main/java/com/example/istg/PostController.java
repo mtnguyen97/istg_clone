8155944d2178ca9d047f986f799dd2422f97ced2
Thế Nguyễn Mai <thenguyen.mai@gmail.com>
2021-05-29 16:44:10 +0700

    add handle invalid Post

--- src/main/java/com/example/istg/PostController.java
+++ src/main/java/com/example/istg/PostController.java
@@ -15,6 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.istg.commons.Post;
@@ -81,6 +85,19 @@ public class PostController {
 			return notFound().build();
 		}
 	}
+	
+	// TODO handle invalid Post
+		@ResponseStatus(HttpStatus.BAD_REQUEST)
+		@ExceptionHandler(MethodArgumentNotValidException.class)
+		public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+			Map<String, String> errors = new HashMap<>();
+			ex.getBindingResult().getAllErrors().forEach((error) -> {
+				String fieldName = ((FieldError) error).getField();
+				String errorMessage = error.getDefaultMessage();
+				errors.put(fieldName, errorMessage);
+			});
+			return errors;
+		}
 
 	// delete post
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
