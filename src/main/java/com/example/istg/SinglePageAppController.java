da4eabcbea7e796b322dbef5582dc1d0909dc9fb
Mai The Nguyen <thenguyen.mai@gmail.com>
2021-09-24 09:07:57 +0700

    single page adding

--- src/main/java/com/example/istg/SinglePageAppController.java
+++ src/main/java/com/example/istg/SinglePageAppController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SinglePageAppController {
-	
-	@RequestMapping(method = RequestMethod.GET, value = {"explore", "message", "auth"})
-	public String dispatchView() {
-		return "index";
-	}
+
+    @RequestMapping(method = RequestMethod.GET, value = {"explore", "message",
+            "auth", "home", "register", "success",
+            "forget", "coverforget", "feed", "messages",
+            "trending", "profile"})
+
+    public String dispatchView() {
+        return "index";
+    }
 
 }
